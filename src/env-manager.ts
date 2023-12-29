4bb1d1eb11e22491d05b13a3a3e89e79c8874e9f
vanisoul <ga2006088445@gmail.com>
2023-12-29 13:40:49 +0800

    Feat : 拿取一次性登入驗證碼完成

---
+++ src/env-manager.ts
@@ -0,0 +1,46 @@
+import { SocksProxyAgent } from "socks-proxy-agent";
+
+// 取得 UserAgent 的資料
+export function getUserAgent() {
+    const mozillaAgent = "Mozilla/5.0"
+    const windowsNT = "Windows NT 10.0; Win64; x64"
+    const chromeAgent = "Chrome/120.0.0.0"
+    const appleWebKitAgent = "AppleWebKit/537.36 (KHTML, like Gecko)"
+    const safariAgent = "Safari/537.36"
+    const userAgent = `${mozillaAgent} (${windowsNT}) ${appleWebKitAgent} ${chromeAgent} ${safariAgent}`
+    return userAgent;
+}
+
+// 取得 SocksProxyAgent 
+export function getSocksProxyAgent() {
+    const socksUrl = process.env["SOCKS_PROXY_URL"] as string;
+    const agent = socksUrl ? new SocksProxyAgent(socksUrl) : undefined;
+    return agent;
+}
+
+// 取得基礎 cookie
+export function getBaseCookie() {
+    const cookie = process.env["CLOUDFLARE_COOKIE"];
+    if (!cookie) {
+        throw new Error("CLOUDFLARE_COOKIE is not defined");
+    }
+    return cookie;
+}
+
+// 取得登入頁面 Url
+export function getLoginPageUrl() {
+    const url = process.env["LOGIN_PAGE_URL"];
+    if (!url) {
+        throw new Error("LOGIN_PAGE_URL is not defined");
+    }
+    return url;
+}
+
+// 取得登入 API Url
+export function getLoginApiUrl() {
+    const url = process.env["LOGIN_API_URL"];
+    if (!url) {
+        throw new Error("LOGIN_API_URL is not defined");
+    }
+    return url;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,95 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
+
+import { getBaseCookie, getLoginApiUrl, getLoginPageUrl, getSocksProxyAgent, getUserAgent } from "./env-manager";
+
+// 發送登入 API 可也接受帳號密碼的輸入函式
+async function login(username: string, password: string) {
+  const url = getLoginApiUrl();
+  const userAgent = getUserAgent();
+  const cookie = getBaseCookie();
+  const agent = getSocksProxyAgent();
+
+  const headers: Record<string, string> = {
+    "User-Agent": userAgent,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  cookie ? headers["Cookie"] = cookie : null;
+
+  // 發送 HTTP POST form-data 請求, 回應此次請求的 SetCookie 回傳
+  const alternativeCaptcha = await getAlternativeCaptcha();
+
+  const body = new URLSearchParams();
+  body.append("userid", username);
+  body.append("password", password);
+  body.append("autoLogin", "T");
+  body.append("alternativeCaptcha", alternativeCaptcha);
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers,
+    body,
+    agent,
+  });
+
+  // 取得 Set-Cookie 的資料
+  const setCookie = response.headers.get("set-cookie");
+  if (!setCookie) {
+    throw new Error("set-cookie not found");
+  }
+  return setCookie;
+}
+
+
+
+// 取得 alternativeCaptcha 的資料
+async function getAlternativeCaptcha() {
+  const loginPageUrl = getLoginPageUrl();
+  const cookie = getBaseCookie();
+  const agent = getSocksProxyAgent();
+
+  const mozillaAgent = "Mozilla/5.0"
+  const windowsNT = "Windows NT 10.0; Win64; x64"
+  const chromeAgent = "Chrome/120.0.0.0"
+  const appleWebKitAgent = "AppleWebKit/537.36 (KHTML, like Gecko)"
+  const safariAgent = "Safari/537.36"
+  const userAgent = `${mozillaAgent} (${windowsNT}) ${appleWebKitAgent} ${chromeAgent} ${safariAgent}`
+
+  const headers: Record<string, string> = {
+    "User-Agent": userAgent
+  };
+  cookie ? headers["Cookie"] = cookie : null;
+
+  // 發送 HTTP 請求
+  const response = await fetch(loginPageUrl, {
+    headers,
+    agent,
+  });
+  const body = await response.text();
+
+  // 使用 jsdom 解析 HTML
+  const dom = new JSDOM(body);
+  const document = dom.window.document;
+
+  // 獲取特定元素的 value
+  const value = document.querySelector<HTMLInputElement>('input[name="alternativeCaptcha"]')?.value;
+  if (!value) {
+    throw new Error("alternativeCaptcha not found");
+  }
+  return value;
+}
+
+
+
+const app = new Elysia()
+  .use(swagger())
+  .get("/getToken", getAlternativeCaptcha)
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
